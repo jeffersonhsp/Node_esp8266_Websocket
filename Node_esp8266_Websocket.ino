68e33ffb7ec1c34f599ea38303547dc41845d76c
Jefferson Paixao <jeffersonhsp@live.com>
2022-05-19 00:29:04 -0300

    name

--- Node_esp8266_Websocket.ino
+++ Node_esp8266_Websocket.ino
@@ -5,20 +5,47 @@
 */
 
 #include <ArduinoWebsockets.h>
-#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-ESP8266WiFiMulti wifiMulti;
+#define LED 2
 
-const char* ssid = "brisa-1394585"; //Enter SSID
-const char* password = "t5tb7ffx"; //Enter Password
+bool conexao_ws   = false;
+int  conexao_wifi = 0;  
+bool conexao_mdns = false;
+bool obrig_ws     = false;
+
+char html_page[] PROGMEM = R"=====(
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+    </head>
+    <body>
+    <h1> Hello word </h1>
+    </body>
+</html>
+ )=====";
+
+
+const char* id = "brisa-1394585"; //Enter SSID
+const char* pass = "t5tb7ffx"; //Enter Password
+
+const char* ssid = "ESP8266"; //Enter SSID
+const char* password = "10203040"; //Enter Password
+
 const char* websockets_server_host = "safe-sea-86700.herokuapp.com"; //Enter server adress
 const uint16_t websockets_server_port = 80; // Enter server port
-bool conexao = false;
+
+
 using namespace websockets;
-#define LED 2
+
+WebsocketsClient client;
+ESP8266WebServer server(80);
+
 
 void onMessageCallback(WebsocketsMessage message) {
     Serial.print("Got Message: ");
@@ -26,12 +53,11 @@ void onMessageCallback(WebsocketsMessage message) {
     if(message.data() == "ON")digitalWrite(LED, LOW);
     Serial.println(message.data());
 }
-
 void onEventsCallback(WebsocketsEvent event, String data) {
     if(event == WebsocketsEvent::ConnectionOpened) {
         Serial.println("Connnection Websocket Opened");
     } else if(event == WebsocketsEvent::ConnectionClosed) {
-        conexao = false;
+        conexao_ws = false;
         Serial.println("Connnection Closed");
     } else if(event == WebsocketsEvent::GotPing) {
         Serial.println("Got a Ping!");
@@ -40,46 +66,50 @@ void onEventsCallback(WebsocketsEvent event, String data) {
     }
 }
 
-WebsocketsClient client;
+
+
+
 void setup() {
     pinMode(LED, OUTPUT);
     
     Serial.begin(115200);
-    Serial.print("Serial.begin");
+    Serial.println("Serial.begin");
     delay(500);
 
   // Don't save WiFi configuration in flash - optional
   WiFi.persistent(false);
   // Set WiFi to station mode
-  WiFi.mode(WIFI_STA);
-  // Register multi WiFi networks
-  wifiMulti.addAP("brisa-", "t5tb7ffx");
-  wifiMulti.addAP("BOKUS 01", "424F4B5553");
-  wifiMulti.addAP("BOKUS 02", "424F4B5553");
-  wifiMulti.addAP("BOKUS 03", "424F4B5553");
-  wifiMulti.addAP("BOKUS 04", "424F4B5553");
-  wifiMulti.addAP("BOKUS 05", "424F4B5553");
-  // More is possible
-  while(wifiMulti.run(3000) != WL_CONNECTED) {
-    Serial.println("WiFi Disconnected");
-    delay(1500);
-  }
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(ssid, password);
+
+/*  // More is possible
   Serial.print("WiFi connected  >> \"");
   Serial.print(WiFi.SSID());
   Serial.print("\" ");
   Serial.println(WiFi.localIP());
 
-
-    // run callback when messages are received
-    client.onMessage(onMessageCallback);
-    // run callback when events are occuring
-    client.onEvent(onEventsCallback);
-
-    //client.send("Hello Server");
+  */
+  server.on("/", []() {
+    server.send(200, "text/html", html_page);
+  });
+  server.begin();
 
 
- ArduinoOTA.setHostname(" OTA ");
-  ArduinoOTA.onStart([]() {
+
+
+
+  // run callback when messages are received
+  client.onMessage(onMessageCallback);
+  // run callback when events are occuring
+  client.onEvent(onEventsCallback);
+  //client.send("Hello Server");
+
+
+
+
+
+ArduinoOTA.setHostname("ESP_PRINCIPAL");
+ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
@@ -111,13 +141,45 @@ void setup() {
     }
   });
   ArduinoOTA.begin();
+  
+  
+
+
+  int currentMillis=0, ledMillis=0, printMillis=0;
+  conexao_wifi = WiFi.begin(id,pass);
+  while(conexao_wifi != WL_CONNECTED  ||  !conexao_mdns  ||  ( !conexao_ws && obrig_ws ) ) {
+          currentMillis = millis();
+          
+          server.handleClient();
+
+          if(currentMillis > ledMillis+50){digitalWrite(LED, !digitalRead(LED));  ledMillis = currentMillis;}
+
+          if(conexao_wifi != WL_CONNECTED)conexao_wifi = WiFi.status();
+          if(!conexao_ws)conexao_ws = client.connect(websockets_server_host, websockets_server_port, "/");
+          if(!conexao_mdns)conexao_mdns = MDNS.begin("principal");
+
+          if(currentMillis > printMillis+1000){
+              Serial.println("WiFi");
+              Serial.println("conexao wifi: "+String(conexao_wifi));
+              Serial.println("conexao ws: "+String(conexao_ws));
+              Serial.println("conexao mdns: "+String(conexao_mdns));
+              Serial.println(" ");
+              printMillis = currentMillis;
+          }
+  }
+
+
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
 
 }
 
 
 void loop() {
+    server.handleClient();
     client.poll();
     ArduinoOTA.handle();
-    while(!conexao){conexao = client.connect(websockets_server_host, websockets_server_port, "/");  Serial.println("Tentando reconexão com websocket");  delay(1000); }
-    while(wifiMulti.run(3000) != WL_CONNECTED) { Serial.print("WiFi Disconnected");  delay(1500);}
+    MDNS.update();
+    while(!conexao_ws){conexao_ws = client.connect(websockets_server_host, websockets_server_port, "/");  Serial.println("Tentando reconexão com websocket");  delay(1000); }
+    while(WiFi.status() != WL_CONNECTED) { WiFi.begin(id,pass); Serial.print("WiFi Disconnected");  delay(6000);}
 }
