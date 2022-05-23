51ec32a7836956350599419655251b6b514b97de
Jefferson Paixao <jeffersonhsp@live.com>
2022-05-23 00:39:11 -0300

    readme...

--- Node_esp8266_Websocket.ino
+++ Node_esp8266_Websocket.ino
@@ -35,6 +35,9 @@ char html_page[] PROGMEM = R"=====(
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
+  <script>
+  //function enviar();
+  </script>
     <style>
       body {
         display: block;
@@ -76,6 +79,16 @@ char html_page[] PROGMEM = R"=====(
         text-decoration: none;
         cursor: pointer;
       }
+   .classname{
+    margin-top: 5%;
+    width: 58vw;
+        margin-left: 5%;
+    font-size: 3vw;
+        border-radius: 6px;
+        padding: 2vw 4vw;
+        cursor: pointer;
+      }
+    
     .divbut{
     align-itens: inline;
     }
@@ -92,16 +105,21 @@ char html_page[] PROGMEM = R"=====(
       <br>
     </form>
   <div class="divbut">
-    <button type="button" class="but" onclick="">Enviar</button>
+    
+  <form id="frm" >
+    <button type="button"  class="but" onclick="enviar()">Enviar</button>
+    <input type="text" class="classname" name="frm1" id="frm1"><br>
+  </form>
+  <br>
     <button type="button" class="but" onclick="limpar()">Limpar</button>
   </div>
   </body>
 
   <script>
+    
     let ws_conect = false;
     let connection = new WebSocket('ws://192.168.4.1:81/'); 
       connection.onopen = function () {  
-      connection.send('Connect ' + new Date()); 
       ws_conect=true;
     }; 
     connection.onclose = function (error) {    
@@ -116,11 +134,20 @@ char html_page[] PROGMEM = R"=====(
       console.log(e.data);
     };
     setInterval(() => {
-      if(ws_conect)connection.send("Request");
-    },500);
+      if(ws_conect)connection.send("#");
+    },100);
 
     document.getElementById("w3review").innerHTML = ""
-
+    
+  function enviar(){
+    var input = document.querySelector("#frm1");
+    var texto = input.value;
+    if(texto != "")connection.send(texto);
+    //if(texto != "")console.log(texto);
+    document.getElementById("frm").reset();
+  }
+  
+  
     function myFunction(t){
       atual = document.getElementById("w3review").innerHTML
       document.getElementById("w3review").innerHTML= atual + t + '\n'
@@ -140,13 +167,16 @@ const char* pass = "t5tb7ffx"; //Enter Password
 const char* ssid = "ESP8266"; //Enter SSID
 const char* password = "10203040"; //Enter Password
 
-const char* WS_SERVER = "safe-sea-86700.herokuapp.com"; //Enter server adress
+//const char* WS_SERVER = "safe-sea-86700.herokuapp.com"; //Enter server adress
+const char* WS_SERVER = "node-red-nippon.herokuapp.com"; //Enter server adress
 const uint16_t WS_PORT = 80; // Enter server port
 
 ESP8266WebServer server(80);
 WebSocketsClient webSocketc;
 WebSocketsServer webSockets = WebSocketsServer(81);
 
+bool flag_socketserial=false;
+String socketserial;
 
 
 //Server
@@ -157,33 +187,41 @@ void webSocketEvents(const uint8_t& num, const WStype_t& type, uint8_t * payload
   switch (type)
   {
     case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
+      //Serial.printf("[%u] Disconnected!\n", num);
       break;
       
     case WStype_CONNECTED:
       {
         IPAddress ip = webSockets.remoteIP(num);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-
+        //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
         // send message to client
-        webSockets.sendTXT(num, "Connected");
+        //webSockets.sendTXT(num, "Connected");
       }
       break;
       
     case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\n", num, payload);
       
-      // send message to client
-      webSockets.sendTXT(num, "json");
+      if(payload[0]=='I' && payload[1]=='p' && payload[2]=='#'){
+        IPAddress broadCast = WiFi.localIP();
+        String x = "ip do esp na rede :"+\
+        String(broadCast[0]) + String(".") +\
+        String(broadCast[1]) + String(".") +\
+        String(broadCast[2]) + String(".") +\
+        String(broadCast[3]); 
+        webSockets.sendTXT(num,x); flag_socketserial=false;
+        break;
+      }
 
-      // send data to all connected clients
-      // webSocket.broadcastTXT("message here");
+
+      if(payload[0] != '#')Serial.printf("%s", payload);
+      if(flag_socketserial) { webSockets.sendTXT(num, "Serial:"+socketserial+"$"); flag_socketserial=false; }
+
+      
       break;
       
     case WStype_BIN:
-      Serial.printf("[%u] get binary length: %u\n", num, length);
-      hexdump(payload, length);
-
+      //Serial.printf("[%u] get binary length: %u\n", num, length);
+      //hexdump(payload, length);
       // send message to client
       // webSocket.sendBIN(num, payload, length);
       break;
@@ -205,23 +243,21 @@ void webSocketEventc(const WStype_t& type, uint8_t * payload, const size_t& leng
     case WStype_DISCONNECTED:
       if (conexao_ws)
       {
-        Serial.println("[WSc] Disconnected!");
+        //Serial.println("[WSc] Disconnected!");
         conexao_ws = false;
       }
       break;
     case WStype_CONNECTED:
       {
         conexao_ws = true;
-        Serial.print("[WSc] Connected to url: ");
-        Serial.println((char *) payload);
+        //Serial.print("[WSc] Connected to url: ");
+        //Serial.println((char *) payload);
 
         // send message to server when Connected
-        webSocketc.sendTXT("Connected");
+        //webSocketc.sendTXT("Connected");
       }
       break;
     case WStype_TEXT:
-
-      
       for(uint8_t i=0;i<sizebuff;i++)pd[i] = 0;
       for(uint8_t i=0;i<sizebuff;i++){
         if(payload[i])pd[i]=payload[i];
@@ -230,7 +266,7 @@ void webSocketEventc(const WStype_t& type, uint8_t * payload, const size_t& leng
 
       myObject = JSON.parse(pd);
       if (JSON.typeof(myObject) == "undefined") {
-        Serial.println("Parsing input failed!");
+        socketserial = "Parsing input failed!";  flag_socketserial = true;
         return;
       }
       
@@ -238,26 +274,27 @@ void webSocketEventc(const WStype_t& type, uint8_t * payload, const size_t& leng
         if(String((const char*) myObject["output1"]) == "ON")digitalWrite(LED, LOW);
         if(String((const char*) myObject["output1"]) == "OFF")digitalWrite(LED, HIGH); 
         
-        Serial.println(timeClient.getFormattedTime());
+        //Serial.println(timeClient.getFormattedTime());
+        socketserial = "Socket: "+timeClient.getFormattedTime();  flag_socketserial = true;
       }
       
       break;
     case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      hexdump(payload, length);
+      //Serial.printf("[WSc] get binary length: %u\n", length);
+      //hexdump(payload, length);
 
       // send data to server
-      webSocketc.sendBIN(payload, length);
+      //webSocketc.sendBIN(payload, length);
       break;
 
     case WStype_PING:
       // pong will be send automatically
-      Serial.printf("[WSc] get ping\n");
+      //Serial.printf("[WSc] get ping\n");
       break;
       
     case WStype_PONG:
       // answer to a ping we send
-      Serial.printf("[WSc] get pong\n");
+      //Serial.printf("[WSc] get pong\n");
       break;
 
     case WStype_ERROR:
@@ -281,7 +318,7 @@ void setup() {
 
     
   Serial.begin(115200);
-  Serial.println("Serial.begin");
+  //Serial.println("Serial.begin");
   delay(500);
 
   // Don't save WiFi configuration in flash - optional
@@ -319,26 +356,26 @@ ArduinoOTA.onStart([]() {
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
+    //Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
+    //Serial.println("\nEnd");
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
+    //Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
+      //Serial.println("Auth Failed");
     } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
+     // Serial.println("Begin Failed");
     } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
+      //Serial.println("Connect Failed");
     } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
+      //Serial.println("Receive Failed");
     } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
+      //Serial.println("End Failed");
     }
   });
   ArduinoOTA.begin();
@@ -358,11 +395,11 @@ ArduinoOTA.onStart([]() {
           if(!conexao_mdns)conexao_mdns = MDNS.begin("principal");
 
           if(currentMillis > printMillis+1000){
-              Serial.println("WiFi");
-              Serial.println("conexao wifi: "+String(conexao_wifi));
-              Serial.println("conexao ws: "+String(conexao_ws));
-              Serial.println("conexao mdns: "+String(conexao_mdns));
-              Serial.println(" ");
+              //Serial.println("WiFi");
+              //Serial.println("conexao wifi: "+String(conexao_wifi));
+              //Serial.println("conexao ws: "+String(conexao_ws));
+              //Serial.println("conexao mdns: "+String(conexao_mdns));
+              //Serial.println(" ");
               printMillis = currentMillis;
           }
   }
@@ -373,7 +410,7 @@ ArduinoOTA.onStart([]() {
 }
 
 uint32_t current_millis = 0;
-
+uint32_t current_millis2 = 0;
 
 void loop() {
   webSocketc.loop();
@@ -381,7 +418,14 @@ void loop() {
   server.handleClient();
   ArduinoOTA.handle();
   MDNS.update();
-  if(millis() > current_millis + 100){timeClient.update(); current_millis = millis();}  //  atualiza a hora a cada 100 millisegundos.
+  
+  if (Serial.available()) {
+    String msg = Serial.readString();
+    socketserial = msg;  flag_socketserial = true;
+  }
+  
+  if(millis() > current_millis + 5000){ webSocketc.sendTXT(timeClient.getFormattedTime());   current_millis = millis();}  //  atualiza a hora a cada 100 millisegundos.
+  if(millis() > current_millis2 + 100){ timeClient.update();   current_millis2 = millis();   }  //  atualiza a hora a cada 100 millisegundos.
   //while(!conexao_ws){conexao_ws = client.connect(websockets_server_host, websockets_server_port, "/");  Serial.println("Tentando reconexão com websocket");  delay(1000); }
   //while(WiFi.status() != WL_CONNECTED) { WiFi.begin(id,pass); Serial.print("WiFi Disconnected");  delay(6000);}
 }
